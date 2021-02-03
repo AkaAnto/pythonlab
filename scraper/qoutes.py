7729dda735aea73ae09203d8de192e7a27a9fff8
Antonio Rodriguez <antojrd@gmail.com>
2021-02-03 04:17:03 +0000

    proogress on spider as a service

--- scraper/qoutes.py
+++ scraper/qoutes.py
@@ -1,14 +1,33 @@
 import scrapy
-
+from scrapy import signals
 #scrapy runspider qoutes.py
 
+
 class QuotesSpider(scrapy.Spider):
-    name = "quotes"
-    start_urls = [
-            'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
-        ]
+    name = 'signals'
+    start_urls = ['http://quotes.toscrape.com/page/1/']
+    items = []
+
+    @classmethod
+    def from_crawler(cls, crawler, *args, **kwargs):
+        spider = super(QuotesSpider, cls).from_crawler(crawler, *args, **kwargs)
+        crawler.signals.connect(spider.item_scraped, signal=signals.item_scraped)
+        crawler.signals.connect(spider.finished, signal=signals.spider_closed)
+        return spider
+
+    def item_scraped(self, item):
+        self.items.append(item)
+        return item
+
+    @staticmethod
+    def finished(spider, reason):
+        spider.log("FINISHED")
+        spider.log(spider.items)
 
     def parse(self, response):
-        # page = response.url.split("/")[-2]
-        print(response.css('title::text'))
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall(),
+            }
--- scraper/test_web_scraper.py
+++ scraper/test_web_scraper.py
@@ -1,8 +1,24 @@
 import unittest
 import pytest
-
+import scrapy
+from scrapy.crawler import CrawlerProcess
+from scraper.qoutes import QuotesSpider
 #pytest test_web_scraper.py
 
+
+
+
+
 class MyTest(unittest.TestCase):
+   
     def test_method1(self):
+        process = CrawlerProcess(settings={
+            "FEEDS": {
+                "items.json": {"format": "json"},
+            },
+        })
+        defer = process.crawl(QuotesSpider)
+        # defer.addCallback(self.finished_callback)
+        process.start()# the script will block here until the crawling is finished
+        
         assert False, "Fail for example"
